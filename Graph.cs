5d81081963d1485c96a0814f3f7b187b5b37b59d
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-08-04 22:22:06 +0300

    Proje dosyası ekle.

---
+++ Graph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    class Graph
+    {
+        private Dictionary<int, List<int>> adjcList;
+
+        public Graph()
+        {
+            adjcList = new Dictionary<int, List<int>>(); 
+        }
+    }
+}
